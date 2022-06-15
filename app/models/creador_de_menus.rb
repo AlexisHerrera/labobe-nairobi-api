bd4f0f7707ec79d1b54dd4f0acbf9ba9db528896
anichu <acai@fi.uba.ar>
2022-06-15 18:51:52 -0300

    Agrego clase CreadorDeMenus y su test inicial

---
+++ app/models/creador_de_menus.rb
@@ -0,0 +1,10 @@
+class CreadorDeMeus
+  def initialize(menu_repo)
+    @repo = menu_repo
+  end
+
+  def crear_menu(id, descripcion, precio)
+    menu_a_crear = Menu.new(id, descripcion, precio)
+    @repo.save(menu_a_crear)
+  end
+end
---
+++ spec/app/models/creador_de_menus.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'CreadorDeMenu' do
+  it 'deberia crear menu y guardar en el repositorio' do
+    repo = instance_double(Persistence::Repositories::MenuRepository).as_null_object
+    menu = Menu.new(11, 'Menu pareja', 1500)
+
+    allow(repo).to receive(:save).with(menu).and_return(menu)
+
+    nuevo_menu = CreadorDeMenu.new(repo).crear_menu(11, 'Menu pareja', 1500)
+    expect(nuevo_menu).to eq(menu)
+  end
+end
