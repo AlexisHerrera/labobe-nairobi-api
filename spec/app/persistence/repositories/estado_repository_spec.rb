edbf80614b2f803c04e47f3b3e0dbec0d3cfd42a
anichu <acai@fi.uba.ar>
2022-06-18 19:33:19 -0300

    Agrego test estado_repository_spec

--- spec/app/persistence/repositories/estado_repository_spec.rb
+++ spec/app/persistence/repositories/estado_repository_spec.rb
@@ -1,20 +1,20 @@
-# require 'integration_helper'
+require 'integration_helper'
 
-# describe Persistence::Repositories::EstadoRepository do
+describe Persistence::Repositories::EstadoRepository do
 
-#   let(:estado_repo) { Persistence::Repositories::EstadoRepository.new }
-#   let(:un_estado) { EstadoDTO.new(0, 'Recibido') }
+   let(:estado_repo) { Persistence::Repositories::EstadoRepository.new }
+   let(:un_estado) { Estado.new(0, 'Recibido') }
 
-#   context 'cuando un menu existe' do
-#     before :each do
-#       @nuevo_estado = estado_repo.save(un_estado)
-#       @estado_id = @nuevo_estado.id
-#     end
+   context 'cuando un menu existe' do
+     before :each do
+       @nuevo_estado = estado_repo.save(un_estado)
+       @estado_id = @nuevo_estado.id
+     end
 
-#     it 'deberia encontrar el estado por id' do
-#       estado = estado_repo.find(@estado_id)
+     it 'deberia encontrar el estado por id' do
+       estado = estado_repo.find(@estado_id)
 
-#       expect(estado.id).to eq(@nuevo_estado.id)
-#     end
-#   end
-# end
+       expect(estado.id).to eq(@nuevo_estado.id)
+     end
+   end
+ end
