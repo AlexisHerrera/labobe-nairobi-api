936ae4d759216bd57b00fa8e5434b7a08a6a1f20
anichu <acai@fi.uba.ar>
2022-06-18 19:37:49 -0300

    Agrego test estado_repository_spec de borrar repositorio

--- spec/app/persistence/repositories/estado_repository_spec.rb
+++ spec/app/persistence/repositories/estado_repository_spec.rb
@@ -5,7 +5,7 @@ describe Persistence::Repositories::EstadoRepository do
    let(:estado_repo) { Persistence::Repositories::EstadoRepository.new }
    let(:un_estado) { Estado.new(0, 'Recibido') }
 
-   context 'cuando un menu existe' do
+   context 'cuando un estado existe' do
      before :each do
        @nuevo_estado = estado_repo.save(un_estado)
        @estado_id = @nuevo_estado.id
@@ -17,4 +17,17 @@ describe Persistence::Repositories::EstadoRepository do
        expect(estado.id).to eq(@nuevo_estado.id)
      end
    end
+
+   context 'cuando un estado existe' do
+    before :each do
+      @nuevo_estado = estado_repo.save(un_estado)
+      @estado_id = @nuevo_estado.id
+    end
+
+    it 'deberia borrar todos los estados' do
+      estado_repo.delete_all
+
+      expect(estado_repo.all.count).to eq(0)
+    end
+  end
  end
