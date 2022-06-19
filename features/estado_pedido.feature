bab62da9e922b034ae7828a5de9cae6e54b44962
fvazquezf <fvazquezf@fi.uba.ar>
2022-06-19 17:29:51 -0300

    cambia estado en el patch de la api

--- features/estado_pedido.feature
+++ features/estado_pedido.feature
@@ -13,24 +13,23 @@ Escenario: 7.0 - Estado de un pedido recien creado es recibido
   Entonces el pedido esta "Recibido"
 
 Escenario: 7.1 - Estado de un pedido de recibido a en preparacion
-  Dado que tengo un pedido "Recibido"
+  Dado que tengo un pedido Recibido
   Cuando cambio el estado del pedido
   Entonces el pedido esta "En preparacion"
 
-@wip
 Escenario: 7.2 - Estado de un pedido de en preparacion a en camino
-  Dado que tengo un pedido "En preparacion"
+  Dado que tengo un pedido En preparacion
   Cuando cambio el estado del pedido
   Entonces el pedido esta "En camino"
 
 @wip
 Escenario: 7.3 - Estado de un pedido de en camino a entregado
-  Dado que tengo un pedido "En camino"
+  Dado que tengo un pedido En camino
   Cuando cambio el estado del pedido
   Entonces el pedido esta "Entregado"
 
 @wip
 Escenario: 7.4 - Estado de un pedido entregado
-  Dado que tengo un pedido "Entregado"
+  Dado que tengo un pedido Entregado
   Cuando cambio el estado del pedido
   Entonces el pedido esta "Entregado"
--- features/step_definitions/estado_pedido_steps.rb
+++ features/step_definitions/estado_pedido_steps.rb
@@ -1,19 +1,18 @@
-Cuando('creo un pedido') do
-  id_menu = 1
-  request = {id_usuario: '123', id_menu: id_menu}.to_json
+Dado('que tengo un pedido Recibido') do
+  request = {id_usuario: '123', id_menu: 1}.to_json
   response = Faraday.post(crear_pedido_url, request, header)
   @pedido = JSON.parse(response.body)
 end
 
-Entonces('el pedido esta {string}') do |estado|
-  id_pedido = @pedido['id_pedido'].to_i
-  response = Faraday.get(consultar_estado_pedido_url, {id: id_pedido}, header)
-  expect(response.status).to eq(200)
-  params = JSON.parse(response.body)
-  expect(params['estado']).to eq estado
+Dado('que tengo un pedido En preparacion') do
+  request = {id_usuario: '123', id_menu: 1}.to_json
+  response = Faraday.post(crear_pedido_url, request, header)
+  @pedido = JSON.parse(response.body)
+  request = {id_pedido: @pedido['id_pedido']}
+  Faraday.patch(crear_pedido_url, request, header)
 end
 
-Dado('que tengo un pedido {string}') do |_string|
+Cuando('creo un pedido') do
   id_menu = 1
   request = {id_usuario: '123', id_menu: id_menu}.to_json
   response = Faraday.post(crear_pedido_url, request, header)
@@ -23,4 +22,12 @@ end
 Cuando('cambio el estado del pedido') do
   request = {id_pedido: @pedido['id_pedido']}
   Faraday.patch(crear_pedido_url, request, header)
+end
+
+Entonces('el pedido esta {string}') do |estado|
+  id_pedido = @pedido['id_pedido'].to_i
+  response = Faraday.get(consultar_estado_pedido_url, {id: id_pedido}, header)
+  expect(response.status).to eq(200)
+  params = JSON.parse(response.body)
+  expect(params['estado']).to eq estado
 end
