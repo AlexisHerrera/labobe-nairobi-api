2d9a6f2820d03c3dd15fad3b88ff3c1b6996366b
Alexis Herrera <alexysaguilar@gmail.com>
2022-07-04 12:00:43 -0300

    Hot-Fix api key weather config

--- scripts/deploy_config.sh
+++ scripts/deploy_config.sh
@@ -3,3 +3,6 @@
 set -x
 
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/config-vars -d @scripts/${CI_ENVIRONMENT_NAME}.config.json -H "Content-Type: application/json"  -H "Accept: application/vnd.heroku+json; version=3" --header "Authorization: Bearer ${HEROKU_TOKEN}"
+
+# Esta feo, pero no pude encontrar la forma de meterlo en el archivo .config.json
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/config-vars -d '{"TOKEN_API_CLIMA":"'"${TOKEN_API_CLIMA}"'"}' -H "Content-Type: application/json"  -H "Accept: application/vnd.heroku+json; version=3" --header "Authorization: Bearer ${HEROKU_TOKEN}"
